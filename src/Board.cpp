1dbfb39f7634c4cd87f39d5718913703d38c3939
jcAlpaca <142179083+jcAlpaca@users.noreply.github.com>
2025-02-11 21:02:55 -0700

    added method to board

--- src/Board.cpp
+++ src/Board.cpp
@@ -8,6 +8,7 @@ class Board {
         uint32_t blackKing;
         uint32_t whiteMan;
         uint32_t whiteKing;
+        bool blackTurn;
         /*
         I am using a 32 bit unsigned binary sequence for each piece type called a bitboard to represent each state.
         The board will be numbered from top to bottom on the dark pieces from 32 to 1, where 1 is on the black side.
@@ -17,6 +18,7 @@ class Board {
             blackKing = 0x00000000;
             whiteMan = 0xFF000000;
             whiteKing = 0x00000000;
+            blackTurn = true;
         }
         /*
         My logic is to use the or operator to add a piece at a 
@@ -32,15 +34,23 @@ class Board {
         void removePiece(int position, uint32_t& bitboard) {
             bitboard &= ~(0x00000001 << (position-1));
         }
-
+        // this is just to swicth whose turn it is
+        void nextTurn() {
+            blackTurn = !blackTurn;
+        }
+        // this returns a boolean for a piece at a given position
         bool pieceAt(int position, uint32_t& bitboard) {
             return (bitboard & (0x00000001 << (position-1))) != 0;
         }
 
+        
+        // this is for debugging the methods
         void printBitboards() {
             std::cout << "Black Men: " << std::bitset<32>(blackMan) << std::endl;
             std::cout << "Black Kings: " << std::bitset<32>(blackKing) << std::endl;
             std::cout << "White Men: " << std::bitset<32>(whiteMan) << std::endl;
             std::cout << "White Kings: " << std::bitset<32>(whiteKing) << std::endl;
         }
+
+
 };
